52d67aab8b12b5d14fcdbe689fd84f9705ae34cf
Andrea Rosero <andrea.rosero.p@gmail.com>
2024-09-01 15:30:27 -0400

    agregandoy ordenando los comentarios

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require("express"); //importamos express
 const app = express(); //instanciamos express
 
-const fs = require("fs");
+const fs = require("fs"); //importamos fs
 
 const port = 3000; //definimos puerto
 
-app.listen(port, () => console.log("Servidor escuchado en puerto 3000!"));
+app.listen(port, () => console.log("Servidor escuchado en puerto 3000!")); //levantamos el servidor
 
-app.use(express.json());
+app.use(express.json()); //middleware
+
+//devuelve la web consultando get con ruta en raíz 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
 app.get("/canciones", (req, res) => {
   const newCancion = JSON.parse(fs.readFileSync("repertorio.json"));
@@ -22,6 +27,7 @@ app.post("/canciones", (req, res) => {
   res.send("Nueva canción agregada");
 });
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+
+//put canciones/:id recibe los datos y actualiza manipulando el json
+
+//delete canciones/:id recibe por querystring el id  elimina del repertorio
