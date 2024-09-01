9e74d60c3ff22ba53cdd0710d9610e97ad7a0e29
Droopytex <a.velasquezvega@gmail.com>
2024-09-01 16:05:53 -0400

    programación y configuración del DELETE

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.listen(port, () => console.log("Servidor escuchado en puerto 3000!")); //lev
 
 app.use(express.json()); //middleware
 
-//devuelve la web consultando get con ruta en raíz 
+//devuelve la web consultando get con ruta en raíz
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
@@ -29,16 +29,16 @@ app.post("/canciones", (req, res) => {
 
 //put canciones/:id recibe los datos y actualiza manipulando el json
 
-app.put("/canciones/:id", (req, res) => {
-  const {id} = req.params;
-  const cancion = req.body; // asigna a variable cancion el body obtenido 
-  const canciones = JSON.parse(fs.readFileSync("repertorio.json")); // lee el json y lo parsea y lo asigna a variable canciones
-  const index = canciones.findIndex(c => c.id == id); //encuentra el id que coincide
-  canciones[index] = cancion;
-  fs.writeFileSync("repertorio.json", JSON.stringify(canciones));
-  res.send("Canción modificada con éxito");
-})
-
-
-
 //delete canciones/:id recibe por querystring el id  elimina del repertorio
+app.delete("/canciones/:id", (req, res) => {
+  const { id } = req.params;
+  let canciones = JSON.parse(fs.readFileSync("repertorio.json"));
+  const filtrarCanciones = caciones.filter((cancion) => cancion.id != id);
+
+  if (filtrarCanciones.length != canciones.length) {
+    fs.writeFileSync("repertorio.json", JSON.stringify(filtrarCanciones));
+    res.send("Canción eliminada");
+  } else {
+    res.status(404).send("Canción no encontrada");
+  }
+});
