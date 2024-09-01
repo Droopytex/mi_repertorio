557bfc8695a1fd429470bdf867b8c81b05582fa2
Droopytex <a.velasquezvega@gmail.com>
2024-09-01 16:19:27 -0400

    ajuste a código

--- index.js
+++ index.js
@@ -30,20 +30,20 @@ app.post("/canciones", (req, res) => {
 //put canciones/:id recibe los datos y actualiza manipulando el json
 
 app.put("/canciones/:id", (req, res) => {
-  const {id} = req.params;
-  const cancion = req.body; // asigna a variable cancion el body obtenido 
+  const { id } = req.params;
+  const cancion = req.body; // asigna a variable cancion el body obtenido
   const canciones = JSON.parse(fs.readFileSync("repertorio.json")); // lee el json y lo parsea y lo asigna a variable canciones
-  const index = canciones.findIndex(c => c.id == id); //encuentra el id que coincide
+  const index = canciones.findIndex((c) => c.id == id); //encuentra el id que coincide
   canciones[index] = cancion;
   fs.writeFileSync("repertorio.json", JSON.stringify(canciones));
   res.send("Canción modificada con éxito");
-})
+});
 
 //delete canciones/:id recibe por querystring el id  elimina del repertorio
 app.delete("/canciones/:id", (req, res) => {
   const { id } = req.params;
   let canciones = JSON.parse(fs.readFileSync("repertorio.json"));
-  const filtrarCanciones = caciones.filter((cancion) => cancion.id != id);
+  const filtrarCanciones = canciones.filter((cancion) => cancion.id != id);
 
   if (filtrarCanciones.length != canciones.length) {
     fs.writeFileSync("repertorio.json", JSON.stringify(filtrarCanciones));
